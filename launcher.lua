c76c8bd6dcc9b7c32225846bd2b76a5aed7c8213
xunuwu <xunuwu@gmail.com>
2025-02-18 13:43:56 +0100

    add toggle to show hidden accounts

--- launcher.lua
+++ launcher.lua
@@ -7,8 +7,8 @@ local sobercookie_command = "sobercookie" -- uses sobercookie from $PATH
 
 local app = Gtk.Application({ application_id = "io.github.xunuwu.sobercookie_launcher" })
 
-local function get_accounts()
-	local handle = io.popen(sobercookie_command .. " list")
+local function get_accounts(show_hidden)
+	local handle = io.popen(sobercookie_command .. " list" .. (show_hidden and " --show-hidden" or ""))
 	if handle == nil then
 		print("failed run command")
 		return
@@ -21,14 +21,9 @@ local function get_accounts()
 	return lines
 end
 
-function app:on_startup()
-	local window = Gtk.ApplicationWindow.new(self)
-	window:set_title("Sobercookie Launcher")
-	window:set_default_size(500, 500)
-	local scrolled = Gtk.ScrolledWindow.new()
+local function account_buttons(show_hidden)
 	local box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 10)
-
-	local accounts = get_accounts()
+	local accounts = get_accounts(show_hidden)
 	if accounts ~= nil then
 		table.insert(accounts, 1, "default")
 		for _, v in ipairs(accounts) do
@@ -40,7 +35,29 @@ function app:on_startup()
 			end
 		end
 	end
+	return box
+end
 
+function app:on_startup()
+	local window = Gtk.ApplicationWindow.new(self)
+	window:set_title("Sobercookie Launcher")
+	window:set_default_size(500, 500)
+	local scrolled = Gtk.ScrolledWindow.new()
+	local box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 10)
+
+	local show_hidden = Gtk.CheckButton.new_with_label("toggle hidden")
+	local toggled = false
+	local acc_btns = account_buttons(false)
+	function show_hidden:on_toggled()
+		toggled = not toggled
+		print(toggled)
+		box:remove(acc_btns)
+		acc_btns = account_buttons(toggled)
+		box:pack_start(acc_btns, false, true, 0)
+		box:show_all()
+	end
+	box:pack_start(show_hidden, false, true, 0)
+	box:pack_start(acc_btns, false, true, 0)
 	box:show_all()
 	scrolled:add(box)
 	scrolled:show()
